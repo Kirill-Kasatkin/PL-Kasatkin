46bb1e95784d1cf69d444c572f3e8cc26ae46884
Kirill-Kasatkin <145608302+Kirill-Kasatkin@users.noreply.github.com>
2023-11-02 23:31:19 +0300

    Add files via upload

---
+++ Практика 7.py
@@ -0,0 +1,117 @@
+"""Вариант 1"""
+
+
+"""Задание 1"""
+
+
+import math
+
+def square_rectangle(a, b):      #Площадь прямоугольника
+    return a * b
+
+def square_triangle(base, height):         #Площадь треугольника
+    return 0.5 * base * height
+
+def square_circle(radius):               #Площадь круга
+    return math.pi * radius**2
+
+def square_square(side):         #Площадь квадрата
+    return side**2
+
+def square_polygon(side, radius, corners):       #Площадь многоугольника
+    return (1/2)*corners*side*radius
+
+def square():       #Пользователь выбирает, какая ему нужна фигура, а затем по ней вводит данные
+    print("Выберите фигуру:")
+    print("1. Прямоугольник")
+    print("2. Треугольник")
+    print("3. Круг")
+    print("4. Квадрат")
+    print("5. Правильный многоугольник")
+
+    choice = input("Введите номер фигуры: ")
+
+    if choice == "1":
+        a = float(input("Введите длину прямоугольника: "))
+        b = float(input("Введите ширину прямоугольника: "))
+        print("Площадь прямоугольника:", square_rectangle(a, b))
+    elif choice == "2":
+        base = float(input("Введите основание треугольника: "))
+        height = float(input("Введите высоту треугольника: "))
+        print("Площадь треугольника:", square_triangle(base, height))
+    elif choice == "3":
+        radius = float(input("Введите радиус круга: "))
+        print("Площадь круга:", square_circle(radius))
+    elif choice == "4":
+        side = float(input("Введите длину стороны квадрата: "))
+        print("Площадь квадрата:", square_square(side))
+    elif choice == "5":
+        side = float(input("Введите кол-во сторон многоугольника: "))
+        radius = float(input("Введите радиус многоугольника: "))
+        corners = float(input("Введите кол-во углов многоугольника: "))
+        print("Площадь многоуольника:", square_polygon(side, radius, corners))
+    else:
+        print("Неверный выбор фигуры!")
+
+square()
+
+"""Задание 2"""
+
+
+def calculate_sum(array):
+    return sum(array)
+
+def calculate_average(array):
+    return sum(array) / len(array)
+
+def calculate_statistics():
+    arrays = []
+
+    for i in range(3):
+        array = []
+        size = int(input(f"Введите размер массива {i+1}: "))
+
+        for j in range(size):
+            element = int(input(f"Введите элемент {j+1} массива {i+1}: "))
+            array.append(element)
+
+        arrays.append(array)
+
+    for i, array in enumerate(arrays):
+        print(f"Массив {i+1}:")
+        print("Сумма элементов:", calculate_sum(array))
+        print("Среднее арифметическое значение:", calculate_average(array))
+        print()
+
+calculate_statistics()
+
+
+
+"""Вариант 10"""
+
+
+
+
+"""Задание 1"""
+def task(a, b, c, n):
+    lst = [2, 2, 1]  # список из цифр
+    count = 0
+    for i1 in lst:  # каждая цифра должна побывать на 1-м месте
+        for i2 in lst:  # каждая цифра должна побывать на 2-м месте
+            for i3 in lst:  # каждая цифра должна побывать на 3-м месте
+                z = i3 * 100 + i2 * 10 + i1  # воспроизводим число
+                if z >= 100 and z <= n:  # проверяем
+                    count += 1
+    return count
+
+print(task(1, 2, 3, 900))
+
+"""Задание 2"""
+def str_rvs (str):
+    word = str.split()  # Разбиваем строку на слова
+    str_rvs  = ' '.join(reversed(word)) # Переворачиваем порядок слов и объединяем их обратно
+    return str_rvs
+
+str = input('Введите строку:')
+res = str_rvs(str) # Вызов ф-ции и вывод результата
+print('Результат:', res)
