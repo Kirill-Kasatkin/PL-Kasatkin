a25bd44ef371bc590c2e60f6b8910543c37e89b6
Kirill-Kasatkin <145608302+Kirill-Kasatkin@users.noreply.github.com>
2023-11-14 23:20:58 +0300

    Update Вар 10.py

--- ПР-7/Вар 10.py
+++ ПР-7/Вар 10.py
@@ -1,30 +1,28 @@
 """Вариант 10"""
 
 
-
-
 """Задание 1"""
-def Y(a, b, c, n):
-    spisok = [4, 2, 7, 9, 6, 3]  # список из цифр
+n = int(input())
+a, b, c = input(), input(), input()
+
+def chisla(a, b, c, N):
     count = 0
-    for n1 in spisok:  # каждая цифра должна побывать на 1-м месте
-        for n2 in spisok:  # каждая цифра должна побывать на 2-м месте
-            for n3 in spisok:  # каждая цифра должна побывать на 3-м месте
-                x = n3 * 100 + n2 * 10 + n1  # воспроизводим число
-                if x >= 100 and x <= n:  # проверяем
-                    count += 1
+    for i in range(100, N + 1):
+        digit = str(i)
+        if a in digit and b in digit and c in digit:
+            count += 1
     return count
 
-print(Y(1, 2, 3, 900))
+print(chisla(a, b, c, n))
+
+
 
 """Задание 2"""
 def REVERSE(stroka):
     word = stroka.split()
-    print(word) # Разбиваем строку на слова
-    str_reverse  = ' '.join(reversed(word))
-    print(str_reverse) # Переворачиваем порядок слов и объединяем их обратно
+    str_reverse  = ' '.join(reversed(word)) 
     return str_reverse
 
 stroka = input('Введите строку:')
-result = REVERSE(stroka) # Вызов ф-ции и вывод результата
+result = REVERSE(stroka) 
 print('Результат:', result)
